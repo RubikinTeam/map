7f657b435cd957b841d40433df14ddf6dcc195ae
duongtruc <duongtruc.92@gmail.com>
2014-07-17 16:35:45 +0700

    Update index.php

---
+++ application/views/home/index.php
@@ -0,0 +1,242 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Starter Template for Bootstrap</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <!--<div class="modal-header">
+        </div>-->
+        <div class="header-menu">
+            <div class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">VMap Project</a>
+                    </div>
+                    <div class="navbar-collapse collapse" style="height: 1px;">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Search</a></li>
+                        </ul>
+                        <!--<form class="navbar-form navbar-left" role="form">
+                            <div class="form-group-lg">
+                                <input type="search" placeholder="Search key" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" placeholder="Password" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-success">Sign in</button>
+                        </form>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-content-master">
+            <div class="container">
+                <div id="sliderb_container" class="sliderb-container">
+                    <div data-u="loading" style="position: absolute; top: 0; left: 0;">
+                        <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block; background-color: #000; top: 0; left: 0;width: 100%;height:100%;">
+                        </div>
+                        <div style="position: absolute; display: block; background: url(img/loading.gif) no-repeat center center; top: 0; left: 0;width: 100%;height:100%;">
+                        </div>
+                    </div>
+                    <div data-u="slides" class="slider">
+                        <div>
+                            <img data-u="image" src="img/landscape/01.jpg">
+                            <div data-u="thumb">Do you notice it is draggable by mouse/finger?</div>
+                        </div>
+                        <div>
+                            <img data-u="image" src="img/landscape/02.jpg">
+                            <div data-u="thumb">Did you drag by either horizontal or vertical?</div>
+                        </div>
+                        <div>
+                            <img data-u="image" src="img/landscape/03.jpg">
+                            <div data-u="thumb">Do you notice navigator responses when drag?</div>
+                        </div>
+                        <div>
+                            <img data-u=image src="img/landscape/04.jpg" />
+                            <div data-u="thumb">Do you notice arrow responses when click?</div>
+                        </div>
+                    </div>
+                    <div data-u="thumbnavigator" class="sliderb-T" style="position: absolute; bottom: 0; left: 0; height:45px; width:600px;">
+                        <div style="filter: alpha(opacity=40); opacity:0.4; position: absolute; display: block; background-color: #000000; top: 0; left: 0; width: 100%; height: 100%;">
+                        </div>
+                        <div data-u="slides">
+                            <div data-u="prototype" style="POSITION: absolute; WIDTH: 600px; HEIGHT: 45px; TOP: 0; LEFT: 0;">
+                                <thumbnailtemplate style="font-family: verdana; font-weight: normal; POSITION: absolute; WIDTH: 100%; HEIGHT: 100%; TOP: 0; LEFT: 0; color:#fff; line-height: 45px; font-size:20px; padding-left:10px;"></thumbnailtemplate>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-u="navigator" class="jssorb01" style="position: absolute; bottom: 16px; right: 10px;">
+                        <div data-u="prototype" style="POSITION: absolute; WIDTH: 12px; HEIGHT: 12px;"></div>
+                    </div>
+                    <span data-u="arrowleft" class="arrowleft jssora05l"></span>
+                    <span data-u="arrowright" class="arrowright jssora05r"></span>
+                </div>
+            </div>
+
+            <div class="main-content container">
+                <div class="content-title">
+                    <span class="pull-left">
+                        <span class="text"><strong>HOẠT ĐỘNG NỔI BẬT</strong></span>
+                    </span>
+                    <span class="pull-right">
+                        <span data-u="arrowleft" class="arrowleft jssora03l">
+                        </span>
+                        <span data-u="arrowright" class="arrowright jssora03r">
+                        </span>
+                    </span>
+                </div>
+                <div id="slider1_container" class="slider1-container">
+
+                    <!-- Loading Screen -->
+                    <div data-u="loading" style="position: absolute; top: 0; left: 0;">
+                        <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
+                background-color: #000; top: 0; left: 0;width: 100%;height:100%;">
+                        </div>
+                        <div style="position: absolute; display: block; background: url(img/loading.gif) no-repeat center center;
+                top: 0; left: 0;width: 100%;height:100%;">
+                        </div>
+                    </div>
+
+                    <!-- Slides Container -->
+                    <div data-u="slides" style="position: absolute; left: 0; top: 0; width: 1140px; height: 450px; overflow: hidden;">
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="main-content container">
+                <div class="content-title">
+                    <span class="pull-left">
+                        <span class="text"><strong>BÀI VIẾT NỔI BẬT</strong></span>
+                    </span>
+                    <span class="pull-right">
+                        <span data-u="arrowleft" class="arrowleft jssora03l">
+                        </span>
+                        <span data-u="arrowright" class="arrowright jssora03r">
+                        </span>
+                    </span>
+                </div>
+                <div id="slider2_container" class="slider1-container">
+
+                    <!-- Loading Screen -->
+                    <div data-u="loading" style="position: absolute; top: 0; left: 0;">
+                        <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
+                background-color: #000; top: 0; left: 0;width: 100%;height:100%;">
+                        </div>
+                        <div style="position: absolute; display: block; background: url(img/loading.gif) no-repeat center center;
+                top: 0; left: 0;width: 100%;height:100%;">
+                        </div>
+                    </div>
+
+                    <!-- Slides Container -->
+                    <div data-u="slides" style="position: absolute; left: 0; top: 0; width: 1140px; height: 450px; overflow: hidden;">
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title"><a href="#">Title 1</a></h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                        <div class="article-box">
+                            <h3 class="article-title">Title 1</h3>
+                            <div class="status"><span class="address">17/7/2014</span></div>
+                            <div class="short-desc">
+                                Fully responsive 3D carousel that allows to display media or HTML content with an unique and original layout. It’s perfect for presentations, for anyone who wants to obtain a great impact on their visitors.
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="modal-footer">
+                <h4 class="text-center"><strong>VMAP PROJECT</strong></h4>
+                <p class="text-center">© 2014 Map Team</p>
+            </div>
+        </div>
+        <script src="js/jquery-2.1.0.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jssor.core.js"></script>
+        <script src="js/jssor.slider.js"></script>
+        <script src="js/jssor.utils.js"></script>
+        <script src="js/slider.js"></script>
+    </body>
+</html>
+=======
+>>>>>>> 5d41589a056aa89e35c2cae49fa4bdba0c04c0c5
