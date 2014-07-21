04af0357d4a6d26cda45dace5b3c2f5b93044ba0
duyen0210 <ntmduyen.fit@gmail.com>
2014-07-21 23:08:19 +0700

    Update comment with Ajax

--- application/controller/activities.php
+++ application/controller/activities.php
@@ -20,22 +20,4 @@ class Activities extends Controller
 
         $this->render('activities/detail', array('activity' => $activity, 'comments' => $comments, 'otherActivities' => $otherActivities));
     }
-
-    public function addComment($id)
-    {
-        if (isset($_POST["submit_add_comment"])) {
-            $comments_model = $this->loadModel('CommentsModel');
-
-            $users_model = $this->loadModel('UsersModel');
-            $userLogged = $users_model->checkUserLogged();
-
-            if($userLogged == 0) {
-                $comments_model->addComment($_POST["type"], $id, 'Anonymous', $_POST["comment"]);
-            }
-            else {
-                $comments_model->addComment($_POST["type"], $id, $userLogged['fname'].' '.$userLogged['lname'], $_POST["comment"]);
-            }
-        }
-        header('location: ' . URL . 'activities/detail/' . $id);
-    }
 }
